0ec8cdfeb5dfe300bce1ae11e27be9a10896498b
Gustavo_Sales <gustavo.sales@liveecommerce.com.br>
2020-04-21 22:25:49 -0300

    Criação de formulario ainda nao terminado

--- src/App/src/Handler/ProductsListHandler.php
+++ src/App/src/Handler/ProductsListHandler.php
@@ -3,12 +3,14 @@
 namespace App\Handler;
 
 use App\Entity\Product;
+use App\Forms\Products\Create as ProductCreateForm;
 use Doctrine\ORM\EntityManager;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Expressive\Template\TemplateRendererInterface;
+use Zend\Hydrator\ClassMethodsHydrator;
 
 class ProductsListHandler implements RequestHandlerInterface
 {
@@ -23,11 +25,19 @@ class ProductsListHandler implements RequestHandlerInterface
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $product = new Product();
+        $form = new ProductCreateForm();
+        $form->setHydrator(new ClassMethodsHydrator());
+        $form->bind(new Product());
 
         $repository = $this->entityManager->getRepository(Product::class);
         $products = $repository->findAll();
 
-        return new HtmlResponse($this->template->render('app::products/list', ['products' => $products]));
+        return new HtmlResponse($this->template->render(
+            'app::products/list',
+            [
+                'form' => $form,
+                'products' => $products
+            ]
+        ));
     }
 }
